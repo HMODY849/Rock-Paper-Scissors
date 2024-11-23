631648078003f9fb2ff263cd30713c1ea76b32cc
HMODY <185634831+HMODY849@users.noreply.github.com.>
2024-11-22 17:25:39 -0800

    edit the console.log to return and clean my js code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let x = "rock";
 let y = "paper";
 let z = "scissor";
 let humanScore = 0;
-let computerScore =0;
+let computerScore = 0;
 function getComputerChoice (){
     let RandoM = Math.floor(Math.random()*3);
     if (RandoM === 0){
@@ -34,73 +34,67 @@ function playRound (humanChoice,computerChoice){
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
     if(humanChoice === x && computerChoice === x){
-        console.log("it's Draw, rock and rock")
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        return "it's Draw, rock and rock Your Score: " +  humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === y &&computerChoice === y){
-        console.log("It's Draw, paper and paper")
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        return "it's Draw, paper and paper Your Score: " +  humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === z && computerChoice === z){
-        console.log("It's Draw, scisser and scisser")
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        return "it's Draw, scisser and scisser Your Score: " +  humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === x && computerChoice === y){
-        console.log("You Lose, paper defeat rock");
-        computerScore ++;
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        computerScore++;
+        return "You Lose, paper defeat rock Your Score: " + humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === x && computerChoice === z){
-        console.log("You Win, rock defeat scisser")
-        humanScore ++;
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        humanScore++;
+        return "You Win, rock defeat scisser Your Score: " + humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === y && computerChoice === x){
-        console.log("You Win, paper defeat rock")
-        humanScore ++;
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        humanScore++;
+        return "You Win, paper defeat rock Your Score: " + humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === y && computerChoice === z){
-        console.log("You Lose, scisser defeat paper")
-        computerScore ++;
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        computerScore++;
+        return "You Lose, scisser defeat paper Your Score: " + humanScore 
+        + "Computer Score: " + computerScore
     }
     else if(humanChoice === z && computerChoice === x){
-        console.log("You Lose, rock defeat scisser")
-        computerScore ++;
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        computerScore++;
+        return "You Lose, rock defeat scisser Your Score: " + humanScore 
+        + "Computer Score: " + computerScore;
     }
     else if(humanChoice === z && computerChoice === y){
-        console.log("You Win, scisser defeat paper")
-        humanScore ++;
-        console.log("Your Score: " + humanScore)
-        console.log("Computer Score: " + computerScore)
+        humanScore++;
+        return "You Win, scisser defeat paper Your Score: " + humanScore 
+        + "Computer Score: " + computerScore;
     }
 }
 
 
 function playGame(){
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
     if(humanScore > computerScore){
-        console.log("Congratulation You Win your score is : " + humanScore + " computer score is : " + computerScore)
+        console.log("Congratulation You Win your score is : " + humanScore 
+            + " computer score is : " + computerScore);
     }
     else if(computerScore > humanScore){
-        console.log("Game over, you lose your score is : " + humanScore + " computer score is : " + computerScore)
+        console.log("Game over, you lose your score is : " + humanScore 
+            + " computer score is : " + computerScore)
     }
     else{
-        console.log("It's Draw your score is : " + humanScore + " computer score is : " + computerScore)
+        console.log("It's Draw your score is : " + humanScore 
+            + " computer score is : " + computerScore)
     }
 }
 playGame();
