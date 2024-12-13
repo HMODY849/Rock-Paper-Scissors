4b9cab06520a67b9df5c4cbc7b13a4d694f74aa3
HMODY <185634831+HMODY849@users.noreply.github.com.>
2024-12-13 01:51:22 -0800

    add images folder and modified html,css,js the audio folder is empty just for now

--- script.js
+++ script.js
@@ -3,98 +3,163 @@ let y = "paper";
 let z = "scissor";
 let humanScore = 0;
 let computerScore = 0;
-function getComputerChoice (){
-    let RandoM = Math.floor(Math.random()*3);
-    if (RandoM === 0){
-        return x;
-    }
-    else if(RandoM === 1){
-        return y;
-    }
-    else{
-        return z;
-    }
-}
- 
-function getHumanChoice(){
-   let human = prompt("chosee your item rock/paper/scissor", "");
+let human="";
+let itemComputer=document.querySelector(".itemComputer");
+let itemHuman=document.querySelector(".itemHuman");
+let paper1= document.querySelector(".item1");
+let rock1= document.querySelector(".item2");
+let scissor1= document.querySelector(".item3");
+let roundResult = document.getElementsByTagName("h1")[0];
+let resultRound1 = document.querySelector(".yourScore");
+let resultRound2 = document.querySelector(".computerScore");
+let playgRound = document.querySelector(".playground");
+let father = document.querySelector("body");
+let finalresult = document.querySelector(".choicePic");
+let Congratulation = document.querySelector(".finalResult");
+let buttonPlayAgain = document.querySelector(".playagin");
+
+rock1.addEventListener("click",function(){
+    human=x;
+    playRound();
+    resultRound1.innerHTML=`${humanScore}`
+    resultRound2.innerHTML=`${computerScore}`
+    winOrlose();
+    });
+
+paper1.addEventListener("click",function(){
+        human=y;
+        playRound();
+        resultRound1.innerHTML=`${humanScore}`
+        resultRound2.innerHTML=`${computerScore}`
+        winOrlose();
+        }); 
+
+scissor1.addEventListener("click",function(){
+        human=z;
+        playRound();
+        resultRound1.innerHTML=`${humanScore}`
+        resultRound2.innerHTML=`${computerScore}`
+        winOrlose();
+        });
+
+function getHumanChoice(human){
     if (human === "rock"){
+        itemHuman.innerHTML='<img src="./images/rock.png">'
         return x;
     }
     else if(human === "paper"){
+        itemHuman.innerHTML='<img src="./images/paper.png">'
         return y;
     }
     else if( human === "scissor"){
+        itemHuman.innerHTML='<img src="./images/scissor.png">'
         return z;
     }
-}
+    }
 
- 
-function playRound (humanChoice,computerChoice){
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    if(humanChoice === x && computerChoice === x){
-        return "it's Draw, rock and rock Your Score: " +  humanScore 
-        + "Computer Score: " + computerScore;
+function getComputerChoice (){
+        let RandoM = Math.floor(Math.random()*3);
+        if (RandoM === 0){
+            itemComputer.innerHTML='<img src="./images/rock.png">'
+            return x;
+        }
+        else if(RandoM === 1){
+            itemComputer.innerHTML='<img src="./images/paper.png">'
+            return y;
+        }
+        else{
+            itemComputer.innerHTML='<img src="./images/scissor.png">'
+            return z;
+        }
     }
-    else if(humanChoice === y &&computerChoice === y){
-        return "it's Draw, paper and paper Your Score: " +  humanScore 
-        + "Computer Score: " + computerScore;
-    }
-    else if(humanChoice === z && computerChoice === z){
-        return "it's Draw, scisser and scisser Your Score: " +  humanScore 
-        + "Computer Score: " + computerScore;
-    }
-    else if(humanChoice === x && computerChoice === y){
-        computerScore++;
-        return "You Lose, paper defeat rock Your Score: " + humanScore 
-        + "Computer Score: " + computerScore;
-    }
-    else if(humanChoice === x && computerChoice === z){
-        humanScore++;
-        return "You Win, rock defeat scisser Your Score: " + humanScore 
-        + "Computer Score: " + computerScore;
-    }
-    else if(humanChoice === y && computerChoice === x){
-        humanScore++;
-        return "You Win, paper defeat rock Your Score: " + humanScore 
-        + "Computer Score: " + computerScore;
-    }
-    else if(humanChoice === y && computerChoice === z){
-        computerScore++;
-        return "You Lose, scisser defeat paper Your Score: " + humanScore 
-        + "Computer Score: " + computerScore
-    }
-    else if(humanChoice === z && computerChoice === x){
-        computerScore++;
-        return "You Lose, rock defeat scisser Your Score: " + humanScore 
-        + "Computer Score: " + computerScore;
-    }
-    else if(humanChoice === z && computerChoice === y){
-        humanScore++;
-        return "You Win, scisser defeat paper Your Score: " + humanScore 
-        + "Computer Score: " + computerScore;
-    }
-}
 
+ function playRound (humanChoice,computerChoice){
+                humanChoice = getHumanChoice(human);
+                computerChoice = getComputerChoice();
+                if(humanChoice === x && computerChoice === x){
+                    roundResult.style.color="gray"
+                    return roundResult.innerHTML="Draw";
+                }
+                else if(humanChoice === y &&computerChoice === y){
+                    roundResult.style.color="gray"
+                    return roundResult.innerHTML="Draw";
+                }
+                else if(humanChoice === z && computerChoice === z){
+                    roundResult.style.color="gray"
+                    return roundResult.innerHTML="Draw";
+                }
+                else if(humanChoice === x && computerChoice === y){
+                    computerScore++;
+                    roundResult.style.color="red"
+                    return roundResult.innerHTML="You Lose";
+                }
+                else if(humanChoice === x && computerChoice === z){
+                    humanScore++;
+                    roundResult.style.color="green"
+                    return roundResult.innerHTML="You Win";
+                }
+                else if(humanChoice === y && computerChoice === x){
+                    humanScore++;
+                    roundResult.style.color="green"
+                    return roundResult.innerHTML="You Win";
+                }
+                else if(humanChoice === y && computerChoice === z){
+                    computerScore++;
+                    roundResult.style.color="red"
+                    return roundResult.innerHTML="You Lose";
+                }
+                else if(humanChoice === z && computerChoice === x){
+                    computerScore++;
+                    roundResult.style.color="red"
+                    return roundResult.innerHTML="You Lose";
+                }
+                else if(humanChoice === z && computerChoice === y){
+                    humanScore++;
+                    roundResult.style.color="green"
+                    return roundResult.innerHTML="You Win";
+                }
+    }
 
-function playGame(){
-    console.log(playRound());
-    console.log(playRound());
-    console.log(playRound());
-    console.log(playRound());
-    console.log(playRound());
-    if(humanScore > computerScore){
-        console.log("Congratulation You Win your score is : " + humanScore 
-            + " computer score is : " + computerScore);
+    function playAgain(){
+        let playagain1 = document.querySelector(".playagin");
+        playagain1.addEventListener("click", function(){
+            humanScore=0;
+            computerScore=0;
+            resultRound1.innerHTML=`${humanScore}`
+            resultRound2.innerHTML=`${computerScore}`
+            itemHuman.innerHTML=``
+            itemComputer.innerHTML=``
+            Congratulation.classList.add("Gameover");
+            buttonPlayAgain.classList.add("Gameover");
+            finalresult.classList.remove("Gameover")
+            paper1.classList.remove("Gameover");
+            rock1.classList.remove("Gameover");
+            scissor1.classList.remove("Gameover");
+        })
     }
-    else if(computerScore > humanScore){
-        console.log("Game over, you lose your score is : " + humanScore 
-            + " computer score is : " + computerScore)
+    
+    function winOrlose (){
+        if(humanScore ==5){
+            roundResult.innerHTML=" "
+            Congratulation.innerHTML='Congratulation you win';
+            Congratulation.classList.remove("Gameover");
+            buttonPlayAgain.classList.remove("Gameover");
+            finalresult.classList.add("Gameover");
+            paper1.classList.add("Gameover");
+            rock1.classList.add("Gameover");
+            scissor1.classList.add("Gameover");
+            playAgain();
+           
+        }else if(computerScore==5){
+            roundResult.innerHTML=" "
+            Congratulation.innerHTML='Game over you lose';
+            Congratulation.classList.remove("Gameover");
+            buttonPlayAgain.classList.remove("Gameover");
+            finalresult.classList.add("Gameover");
+            paper1.classList.add("Gameover");
+            rock1.classList.add("Gameover");
+            scissor1.classList.add("Gameover");
+            playAgain();
+        }
     }
-    else{
-        console.log("It's Draw your score is : " + humanScore 
-            + " computer score is : " + computerScore)
-    }
-}
-playGame();
+    
